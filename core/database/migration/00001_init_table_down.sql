1953e9a876a35cf2cb369ae664c485b0e66ee3e6
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2023-12-09 05:50:38 +0700

    feat: configure app (#1)
    * feat: panic handler
    
    * feat: db migration
    
    * feat: general web response
    
    * feat: configure app

---
+++ core/database/migration/00001_init_table_down.sql
@@ -0,0 +1,23 @@
+-- Pemeriksaan
+DROP TABLE IF EXISTS pemeriksaan;
+
+-- Jadwal Penyuluhan
+DROP TABLE IF EXISTS jadwal_penyuluhan;
+
+-- Jadwal Posyandu
+DROP TABLE IF EXISTS jadwal_posyandu;
+
+-- Posyandu
+DROP TABLE IF EXISTS posyandu;
+
+-- Kader
+DROP TABLE IF EXISTS kader;
+
+-- Remaja
+DROP TABLE IF EXISTS remaja;
+
+-- Bidan
+DROP TABLE IF EXISTS bidan;
+
+-- Users
+DROP TABLE IF EXISTS users;
