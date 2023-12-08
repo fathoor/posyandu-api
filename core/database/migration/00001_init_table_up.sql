78efb625015ab12078d049ddcac304ed313c7cf3
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2023-12-09 05:43:18 +0700

    feat: db migration

---
+++ core/database/migration/00001_init_table_up.sql
@@ -0,0 +1,101 @@
+-- Users
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nama VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    nik BIGINT NOT NULL UNIQUE,
+    tempat_lahir VARCHAR(255) NOT NULL,
+    tanggal_lahir DATE NOT NULL,
+    alamat VARCHAR(255) NOT NULL,
+    provinsi VARCHAR(255) NOT NULL,
+    kota VARCHAR(255) NOT NULL,
+    kecamatan VARCHAR(255) NOT NULL,
+    kelurahan VARCHAR(255) NOT NULL,
+    kode_pos INT NOT NULL,
+    rt INT NOT NULL,
+    rw INT NOT NULL,
+    telepon VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'bidan', 'kader', 'remaja') NOT NULL
+) ENGINE = InnoDB;
+
+-- Bidan
+CREATE TABLE bidan (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    jabatan ENUM('terampil, mahir, penyelia') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+-- Posyandu
+CREATE TABLE posyandu (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    bidan_id INT NOT NULL,
+    nama VARCHAR(255) NOT NULL,
+    alamat VARCHAR(255) NOT NULL,
+    provinsi VARCHAR(255) NOT NULL,
+    kota VARCHAR(255) NOT NULL,
+    kecamatan VARCHAR(255) NOT NULL,
+    kelurahan VARCHAR(255) NOT NULL,
+    kode_pos INT NOT NULL,
+    rt INT NOT NULL,
+    rw INT NOT NULL,
+    FOREIGN KEY (bidan_id) REFERENCES bidan(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+-- Remaja
+CREATE TABLE remaja (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    posyandu_id INT NOT NULL,
+    nama_ayah VARCHAR(255) NOT NULL,
+    nama_ibu VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (posyandu_id) REFERENCES posyandu(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+-- Kader
+CREATE TABLE kader (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+-- Jadwal Posyandu
+CREATE TABLE jadwal_posyandu (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    posyandu_id INT NOT NULL,
+    waktu_mulai DATE NOT NULL,
+    waktu_selesai DATE NOT NULL,
+    FOREIGN KEY (posyandu_id) REFERENCES posyandu(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+-- Jadwal Penyuluhan
+CREATE TABLE jadwal_penyuluhan (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    posyandu_id INT NOT NULL,
+    waktu_mulai DATE NOT NULL,
+    waktu_selesai DATE NOT NULL,
+    materi TEXT,
+    FOREIGN KEY (posyandu_id) REFERENCES posyandu(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+-- Pemeriksaan
+CREATE TABLE pemeriksaan (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    bidan_id INT NOT NULL,
+    remaja_id INT NOT NULL,
+    berat_badan FLOAT,
+    tinggi_badan FLOAT,
+    tekanan_darah FLOAT,
+    lingkar_lengan FLOAT,
+    tingkat_glukosa FLOAT,
+    kadar_hemoglobin FLOAT,
+    pemberian_fe BOOL NOT NULL,
+    waktu_pengukuran DATE NOT NULL,
+    kondisi_umum ENUM('baik', 'cukup', 'lemah') NOT NULL,
+    keterangan TEXT,
+    FOREIGN KEY (bidan_id) REFERENCES bidan(id) ON DELETE CASCADE,
+    FOREIGN KEY (remaja_id) REFERENCES remaja(id) ON DELETE CASCADE
+) ENGINE = InnoDB;
