9d0e0f4e551c8d68c183e357e4b9927fe71a19fd
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2023-12-09 15:24:15 +0700

    feat: user provider

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/itsLeonB/posyandu-api/core/config"
 	"github.com/itsLeonB/posyandu-api/core/exception"
+	"github.com/itsLeonB/posyandu-api/core/provider"
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -10,8 +11,10 @@ func main() {
 	var (
 		cfg = config.ProvideConfig()
 		app = config.ProvideApp(cfg)
-		_   = config.ProvideDB(cfg)
+		db  = config.ProvideDB(cfg)
 	)
+
+	provider.ProvideModule(app, db)
 
 	err := app.Listen(cfg.Get("APP_ADDRESS"))
 	exception.PanicIfNeeded(err)
