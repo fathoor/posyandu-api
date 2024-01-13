62ce06fe16e0df0406ab4b6efff2af6adcecf4b5
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-13 16:03:31 +0700

    feat: remaja service

---
+++ module/remaja/service/remaja_service.go
@@ -0,0 +1,12 @@
+package service
+
+import "github.com/itsLeonB/posyandu-api/module/remaja/model"
+
+type RemajaService interface {
+	Create(request *model.RemajaCreateRequest) (model.RemajaResponse, error)
+	GetAll() ([]model.RemajaResponse, error)
+	GetByID(id int) (model.RemajaResponse, error)
+	Update(id int, request *model.RemajaUpdateRequest) (model.RemajaResponse, error)
+	UpdateKader(id int, request *model.RemajaUpdateKaderRequest) (model.RemajaResponse, error)
+	Delete(id int) error
+}
