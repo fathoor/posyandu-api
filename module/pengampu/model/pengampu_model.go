939bdc2cac4b839f17985140f0d5dfaa9600ef10
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-13 18:33:59 +0700

    feat: pengampu entity and model

---
+++ module/pengampu/model/pengampu_model.go
@@ -0,0 +1,39 @@
+package model
+
+type PengampuCreateRequest struct {
+	BidanID    int `json:"bidan_id" validate:"required"`
+	PosyanduID int `json:"posyandu_id" validate:"required"`
+}
+
+type PengampuUpdateRequest struct {
+	BidanID    int `json:"bidan_id" validate:"required"`
+	PosyanduID int `json:"posyandu_id" validate:"required"`
+}
+
+type PengampuBidanUserResponse struct {
+	ID           int    `json:"id"`
+	Nama         string `json:"nama"`
+	NIK          int64  `json:"nik"`
+	TanggalLahir string `json:"tanggal_lahir"`
+	Foto         string `json:"foto"`
+	Role         string `json:"role"`
+}
+
+type PengampuBidanResponse struct {
+	ID      int                       `json:"id"`
+	User    PengampuBidanUserResponse `json:"user"`
+	Jabatan string                    `json:"jabatan"`
+}
+
+type PengampuPosyanduResponse struct {
+	ID     int    `json:"id"`
+	Nama   string `json:"nama"`
+	Alamat string `json:"alamat"`
+	Foto   string `json:"foto"`
+}
+
+type PengampuResponse struct {
+	ID       int                      `json:"id"`
+	Bidan    PengampuBidanResponse    `json:"bidan"`
+	Posyandu PengampuPosyanduResponse `json:"posyandu"`
+}
