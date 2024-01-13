f6d263fecd23c7874625c0c9deca3d724b6eeb2f
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-13 09:44:57 +0700

    feat: route bidan endpoint (#6)
    * feat: bidan entity and model
    
    * feat: bidan repository
    
    * feat: bidan service
    
    * feat: bidan validation
    
    * feat: bidan controller
    
    * feat: bidan provider
    
    * feat: apply unique to `user_id`

--- core/provider/provider.go
+++ core/provider/provider.go
@@ -2,6 +2,9 @@ package provider
 
 import (
 	"github.com/gofiber/fiber/v2"
+	bidanController "github.com/itsLeonB/posyandu-api/module/bidan/controller"
+	bidanRepository "github.com/itsLeonB/posyandu-api/module/bidan/repository"
+	bidanService "github.com/itsLeonB/posyandu-api/module/bidan/service"
 	userController "github.com/itsLeonB/posyandu-api/module/user/controller"
 	userRepository "github.com/itsLeonB/posyandu-api/module/user/repository"
 	userService "github.com/itsLeonB/posyandu-api/module/user/service"
@@ -10,12 +13,22 @@ import (
 
 func ProvideModule(app *fiber.App, db *gorm.DB) {
 	ProvideUser(app, db)
+	ProvideBidan(app, db)
 }
 
 func ProvideUser(app *fiber.App, db *gorm.DB) {
 	repository := userRepository.ProvideUserRepository(db)
 	service := userService.ProvideUserService(&repository)
 	controller := userController.ProvideUserController(&service)
+
+	controller.Route(app)
+}
+
+func ProvideBidan(app *fiber.App, db *gorm.DB) {
+	bidanRepo := bidanRepository.ProvideBidanRepository(db)
+	userRepo := userRepository.ProvideUserRepository(db)
+	service := bidanService.ProvideBidanService(&bidanRepo, &userRepo)
+	controller := bidanController.ProvideBidanController(&service)
 
 	controller.Route(app)
 }
