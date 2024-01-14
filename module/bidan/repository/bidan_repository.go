e721feddf054bc7d862bbb1aa546c610533b15ef
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-14 19:03:33 +0700

    feat: update bidan repository

--- module/bidan/repository/bidan_repository.go
+++ module/bidan/repository/bidan_repository.go
@@ -6,6 +6,7 @@ type BidanRepository interface {
 	Insert(bidan *entity.Bidan) error
 	FindAll() ([]entity.Bidan, error)
 	FindByID(id int) (entity.Bidan, error)
+	FindByUserID(userID int) (entity.Bidan, error)
 	Save(bidan *entity.Bidan) error
 	Delete(bidan *entity.Bidan) error
 }
