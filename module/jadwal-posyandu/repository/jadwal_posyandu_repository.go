3767a3d624755bcab6162bc1576a9cd4d460dc39
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-14 11:16:09 +0700

    feat: route jadwal posyandu endpoint (#12)
    * feat: jadwal posyandu entity and model
    
    * feat: jadwal posyandu repository
    
    * feat: jadwal posyandu service
    
    * feat: jadwal posyandu validation
    
    * feat: jadwal posyandu controller
    
    * feat: jadwal posyandu provider

---
+++ module/jadwal-posyandu/repository/jadwal_posyandu_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "github.com/itsLeonB/posyandu-api/module/jadwal-posyandu/entity"
+
+type JadwalPosyanduRepository interface {
+	Insert(jadwalPosyandu *entity.JadwalPosyandu) error
+	FindAll() ([]entity.JadwalPosyandu, error)
+	FindByID(id int) (entity.JadwalPosyandu, error)
+	Save(jadwalPosyandu *entity.JadwalPosyandu) error
+	Delete(jadwalPosyandu *entity.JadwalPosyandu) error
+}
