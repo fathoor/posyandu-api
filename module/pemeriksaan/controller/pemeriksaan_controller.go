9ba20bd0e2024a7c5b7f24943816477e03c186ab
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-14 21:37:11 +0700

    feat: update pemeriksaan controller

--- module/pemeriksaan/controller/pemeriksaan_controller.go
+++ module/pemeriksaan/controller/pemeriksaan_controller.go
@@ -6,6 +6,7 @@ type PemeriksaanController interface {
 	Route(app *fiber.App)
 	Create(ctx *fiber.Ctx) error
 	GetAll(ctx *fiber.Ctx) error
+	GetAllByRemajaID(ctx *fiber.Ctx) error
 	GetByID(ctx *fiber.Ctx) error
 	Update(ctx *fiber.Ctx) error
 	Delete(ctx *fiber.Ctx) error
