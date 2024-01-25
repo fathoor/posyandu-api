9e42c4a7000e30a71f4ec1ca64531443cd3c5f21
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-01-25 08:44:04 +0700

    fix: authorize bidan access for pemeriksaan (#42)

--- module/pemeriksaan/service/pemeriksaan_service.go
+++ module/pemeriksaan/service/pemeriksaan_service.go
@@ -5,7 +5,7 @@ import "github.com/itsLeonB/posyandu-api/module/pemeriksaan/model"
 type PemeriksaanService interface {
 	Create(request *model.PemeriksaanCreateRequest) (model.PemeriksaanResponse, error)
 	GetAll() ([]model.PemeriksaanResponse, error)
-	GetAllByRemajaID(id int) ([]model.PemeriksaanResponse, error)
+	GetByRemajaUserID(id int) ([]model.PemeriksaanResponse, error)
 	GetByID(id int) (model.PemeriksaanResponse, error)
 	Update(id int, request *model.PemeriksaanUpdateRequest) (model.PemeriksaanResponse, error)
 	Delete(id int) error
